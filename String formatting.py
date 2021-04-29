4e158b16806b7f532696782475f1d9542ed62703
Gridushko Anton Anatolievich <Anton_Hrudziushka@epam.com>
2021-04-29 04:55:34 +0300

    Add file with string formatting variances. Get 3 new of them.

---
+++ String formatting.py
@@ -0,0 +1,6 @@
+print('Hello, my name is {0}'.format('Anton')) # here we use format word
+print('{}, {}{}'.format('Hello','Anton','!')) # here we insert automatically
+print('%s, %s%s'% ('Hello','Irina','!')) # without format
+
+name = 'Arina'
+print(f'My name is {name}') #dynamic or late binding
